7c70c71c181dc1d25aaf5960c5f0537a697a7bb6
Evgeniy <bridskijevgeny@yandex.ru>
2022-04-03 23:19:16 +0300

    Создан цикл, который позволяет генерировать массив с заданным количеством элементов

--- Final_work/Program.cs
+++ Final_work/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше, либо равна 3 символа.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+
+Console.WriteLine("Введите желаемое количество элементов в массиве: ");
+int a = int.Parse(Console.ReadLine());
+string [] Array = new string [a];
+
+for(int i = 0; i<Array.Length; i++)
+{
+    Array[i] = Console.ReadLine();
+    
+} 
